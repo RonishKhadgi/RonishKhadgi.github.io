af4873a05c7f874d50221671104b48761a6af9d3
RonishKhadgi <60719674+RonishKhadgi@users.noreply.github.com>
2020-03-04 22:27:50 -0500

    update

---
+++ lab_4/scripts/main.js
@@ -0,0 +1,5 @@
+const header = document.querySelector(".top");
+header.innerHTML = "Ronish Khadgi's Lab 4";
+
+var paragraphOne = document.createElement("p");
+var textOne = document.createTextNode("My name has six characters");

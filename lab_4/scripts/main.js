3bfd9b1d7a0be24dd401d7e0ba3bf80eb024d9c1
RonishKhadgi <60719674+RonishKhadgi@users.noreply.github.com>
2020-03-06 09:36:48 -0500

    update

--- lab_4/scripts/main.js
+++ lab_4/scripts/main.js
@@ -3,3 +3,21 @@ header.innerHTML = "Ronish Khadgi's Lab 4";
 
 var paragraphOne = document.createElement("p");
 var textOne = document.createTextNode("My name has six characters");
+paragraphOne.appendChild(textOne);
+
+var paragraphTwo = document.createElement("p");
+var textTwo = document.createTextNode("The third character of my name is N");
+paragraphTwo.appendChild(textTwo);
+
+var breakLine = document.createElement("br");
+paragraphTwo.appendChild(breakLine);
+var textThree = document.createTextNode("ish");
+paragraphTwo.appendChild(textThree);
+
+function clickMe() {
+    const content = document.querySelector(".content");
+    content.innerHTML = "Hello world";     
+    content.appendChild(paragraphOne);
+    content.appendChild(paragraphTwo);
+  }
+
